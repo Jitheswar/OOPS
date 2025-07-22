6ef25a25515508f468702129304044b9b1b9b664
Jitheswar <jitheswaredu@gmail.com>
2025-07-22 14:23:50 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,13 @@
+package mypackage;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int X=10;
+        int Y=20;
+        int Z = X + Y;
+        System.out.println(Z);
+	}
+
+}
